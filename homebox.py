90b687549f21277077b92c4c98c8c724a9511673
ronbruins <43776917+ronbruins@users.noreply.github.com>
2024-06-01 08:22:53 +0300

    added comment 1

--- homebox.py
+++ homebox.py
@@ -18,6 +18,7 @@ def main():
     #label_id = "2070e5f5-7e53-4aa6-bf87-a07b9e3b0d18"
     #update_label(label_id)
 
+## main loop for the files ##
 def do_location_folder():
     loc_idx = 0
     for root, dirs, files in os.walk(loc_folder):
